8b63e0bb274453db8a546db1cd43814cfaa0abed
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2023-04-15 11:51:46 +0100

    FS-1: install express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World')
+import express from 'express'
+
+const app = express()
+
+const PORT = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
